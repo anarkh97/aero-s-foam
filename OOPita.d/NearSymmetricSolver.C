28677128269fbb28a751d10dbbe919ef10c4aa3b
Julien Cortial <jcortial@stanford.edu>
2009-06-21 14:56:06 -0700

    Added NearSymmetricSolver (LU factorization with symmetric pivoting)

---
+++ OOPita.d/NearSymmetricSolver.C
@@ -0,0 +1,161 @@
+#include "NearSymmetricSolver.h"
+
+#include <algorithm>
+#include <iterator>
+
+extern "C" {
+  // Lapack: Matrix-matrix multiplication
+  // C := alpha*op(A)*op(B) + beta*C where op(M) = M or M^T
+  /*void _FORTRAN(dgemm)(const char* transa, const char* transb, const int* m, const int* n, const int* k,
+                       const double* alpha, const double* a, const int* lda, const double* b, const int* ldb,
+                       const double* beta, double* c, const int* ldc);*/
+
+  // Blas: Rank-1 update A += alpha*x*y'
+  void _FORTRAN(dger)(const int* m, const int* n, const double* alpha,
+                      const double* x, const int* incx, const double* y, const int* incy,
+                      double* a, const int* lda);
+
+  // Blas: Triangular system resolution
+  void _FORTRAN(dtrsv)(const char * uplo, const char * trans, const char * diag, const int * n,
+                       const double * a, const int * lda, double * x, const int * incx);
+
+  // Lapack: Row pivoting
+  void _FORTRAN(dlaswp)(const int* n, double* a, const int* lda, const int* k1, const int* k2,
+                        const int* ipiv, const int* incx);
+
+  // Blas: Column (vector) interchange
+  void _FORTRAN(dswap)(const int* n, double* x, const int* incx, double* y, const int* incy);
+
+  // Blas: Vector scaling
+  void _FORTRAN(dscal)(const int* n, const double* da, double* dx, const int* incx);
+
+}
+
+namespace Pita {
+
+NearSymmetricSolver::NearSymmetricSolver(double tol) :
+  RankDeficientSolver(),
+  tolerance_(tol),
+  transposedMatrix_()
+{}
+
+void
+NearSymmetricSolver::toleranceIs(double tol) {
+  // TODO
+  setTolerance(tol);
+}
+
+void
+NearSymmetricSolver::transposedMatrixIs(const FullSquareMatrix & tm) {
+  transposedMatrix_.copy(tm);
+  
+  setMatrixSize(transposedMatrix_.dim());
+  setFactorRank(0);
+  setStatus(NON_FACTORIZED);
+}
+
+void
+NearSymmetricSolver::statusIs(RankDeficientSolver::Status s) {
+  if (status() == s) {
+    return;
+  }
+
+  if (s == FACTORIZED) {
+    // Initialize permutation
+    getFactorPermutation().sizeIs(matrixSize());
+    for (int i = 0; i < matrixSize(); ++i) {
+      getFactorPermutation()[i] = i + 1;
+    }
+    pivots_.sizeIs(matrixSize());
+
+    // Initialize diagonal pivot values
+    SimpleBuffer<double> pivot_values;
+    pivot_values.sizeIs(matrixSize());
+
+    // Numerical constant
+    const int int_one = 1;
+    const double minus_one = -1;
+
+    double first_pivot = 0.0;
+    
+    int k;
+    for (k = 0; k < matrixSize(); ++k) {
+      // Update pivot values, find largest and permute
+      for (int i = 0; i < matrixSize(); ++i) {
+        pivot_values[i] = transposedMatrix()[i][i];
+      }
+      double * head_pivot = pivot_values.array() + k;
+      double * max_pivot = std::max_element(head_pivot, pivot_values.array() + matrixSize());
+      const int p = std::distance(pivot_values.array(), max_pivot);
+      pivots_[k] = p + 1;
+      std::swap(head_pivot, max_pivot);
+      std::swap(getFactorPermutation()[k], getFactorPermutation()[p]);
+      
+      if (k == 0) {
+        first_pivot = *head_pivot;
+      }
+
+      // Perform symmetric permutation
+      const int current_n = matrixSize() - k;
+      _FORTRAN(dswap)(&current_n, &transposedMatrix_[k][k], &int_one, &transposedMatrix_[p][k], &int_one);
+      _FORTRAN(dswap)(&current_n, &transposedMatrix_[k][k], &getMatrixSize(), &transposedMatrix_[k][p], &getMatrixSize());
+      
+      // Check for singularity
+      if (*max_pivot <= first_pivot * tolerance()) {
+        break;
+      }
+
+      // Compute column of L
+      const double pivot_inverse = 1.0 / *head_pivot;
+      const int current_n_minus_one = current_n - 1;
+      _FORTRAN(dscal)(&current_n_minus_one, &pivot_inverse, &transposedMatrix_[k][k+1], &int_one);
+
+      // Update remaining block of A
+      _FORTRAN(dger)(&current_n_minus_one, &current_n_minus_one, &minus_one,
+                     &transposedMatrix_[k][k+1], &int_one, &transposedMatrix_[k+1][k], &getMatrixSize(),
+                     &transposedMatrix_[k+1][k+1], &getMatrixSize());
+    }
+
+    setFactorRank(k);
+  }
+
+  setStatus(s);
+}
+
+const Vector &
+NearSymmetricSolver::solution(Vector & rhs) const {
+  if (rhs.size() != matrixSize()) {
+    throw Fwk::RangeException("in NearSymmetricSolver::solution - Size mismatch"); 
+  }
+
+  if (status() != FACTORIZED) {
+    throw Fwk::RangeException("in NearSymmetricSolver::solution - Non-factorized matrix");
+  }
+ 
+  // 1) rhs <- P * rhs
+  const int int_one = 1;
+  _FORTRAN(dlaswp)(&int_one, rhs.data(), &getMatrixSize(), &int_one, &getFactorRank(),
+                   pivots_.array(), &int_one);
+
+  // 2) rhs <- L^{-1} * rhs
+  const char lower = 'L';
+  const char non_trans = 'N';
+  const char unit_diag = 'U';
+  _FORTRAN(dtrsv)(&lower, &non_trans, &unit_diag, &getFactorRank(), transposedMatrix().data(),
+                  &getMatrixSize(), rhs.data(), &int_one);
+
+  // 3) rhs <- U^{-1} * rhs
+  const char upper = 'U';
+  const char non_unit_diag = 'N';
+  _FORTRAN(dtrsv)(&upper, &non_trans, &non_unit_diag, &getFactorRank(), transposedMatrix().data(),
+                  &getMatrixSize(), rhs.data(), &int_one);
+
+  // 4) rhs <- P * rhs
+  const int int_minus_one = -1;
+  _FORTRAN(dlaswp)(&int_one, rhs.data(), &getMatrixSize(), &int_one, &getFactorRank(),
+                   pivots_.array(), &int_minus_one);
+
+  return rhs;
+}
+
+} /* end namespace Pita */

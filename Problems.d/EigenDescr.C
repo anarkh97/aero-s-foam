6240eec8b9cacf45c4742746ecb41fd43e9bf96b
Philip Avery <pavery@stanford.edu>
2010-03-16 13:33:51 -0700

    addedmass 2 option

--- Problems.d/EigenDescr.C
+++ Problems.d/EigenDescr.C
@@ -13,6 +13,7 @@
 #include <Math.d/Skyline.d/SkyMatrix.h>
 #include <Math.d/Vector.h>
 #include <Math.d/VectorSet.h>
+#include <Math.d/AddedMassMatrix.h>
 
 #include <Solvers.d/Solver.h>
 #include <Solvers.d/Rbm.h>
@@ -106,9 +107,10 @@ SingleDomainEigen::buildEigOps( DynamMat &dMat )
 {
  AllOps<double> allOps;
 
- //if(addedMass) allOps.M = domain->constructDBSparseMatrix<double>((DofSetArray *)0, nodeToNode_added);
- //else 
- allOps.M = domain->constructDBSparseMatrix<double>();
+ if(domain->solInfo.addedMass == 2) allOps.M = new AddedMassMatrix<double>(domain->getDSA(), domain->getCDSA(), domain->nodeToNode,
+                                                                           domain->Mff, domain, Domain::multC, Domain::trMultC);
+ else 
+   allOps.M = domain->constructDBSparseMatrix<double>();
  // Used for printing out K during debugging.
  allOps.K = domain->constructDBSparseMatrix<double>();
 

fea0f2efd37bb188feb3056f60ec1771bf29beae
Philip Avery <pavery@stanford.edu>
2010-07-19 13:14:03 -0700

    bt shell update

--- Element.d/NonLinearity.d/ElastoViscoPlasticMat.h
+++ Element.d/NonLinearity.d/ElastoViscoPlasticMat.h
@@ -8,8 +8,9 @@ class ElastoViscoPlasticMat : public NLMaterial
   public:
     double ematpro[8]; // Young's modulus, Poisson's ratio, mass density, etc
 
-    ElastoViscoPlasticMat(double rho, double E, double nu) // TODO add the other parameters
-      { ematpro[0] = E; ematpro[1] = nu; ematpro[2] = rho; }
+    ElastoViscoPlasticMat(double rho, double E, double nu, double p4, double p5, double p6, double p7, double p8)
+      { ematpro[0] = E; ematpro[1] = nu; ematpro[2] = rho; ematpro[3] = p4; 
+        ematpro[4] = p5; ematpro[5] = p6; ematpro[6] = p7; ematpro[7] = p8; }
 
     int getNumStates() { return 0; }
 

98b30555d5260fcba3191de4cebf98057d5fc0f8
Julien Cortial <jcortial@stanford.edu>
2010-12-01 10:27:17 -0800

    Fixed output for nonlinear PITA - Fixed crash bug when outputting energies - Fixed inconsistent output

--- Driver.d/NLStatic.C
+++ Driver.d/NLStatic.C
@@ -672,7 +672,7 @@ Domain::postProcessingImpl(int iInfo, GeomState *geomState, Vector& force, Vecto
       if (sinfo.probType == SolverInfo::NonLinDynam) {
         double dW=0.0, dWaero = 0.0, Wela=0.0, Wkin=0.0;
 
-        if(time==sinfo.initialTime) {
+        if(!previousExtForce) {
           Wext=0.0;
           Waero=0.0;
           Wdmp=0.0;
@@ -739,7 +739,7 @@ Domain::postProcessingImpl(int iInfo, GeomState *geomState, Vector& force, Vecto
       pWela=Wela;
       pWkin=Wkin;
 
-      if(time==sinfo.initialTime) {
+      if(!previousDisp) {
         previousDisp = new Vector(sol);
       } else {
         (*previousExtForce) = force;

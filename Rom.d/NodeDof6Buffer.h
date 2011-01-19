abb0c72a7175df5d8ed4d9b9c3d727c6d171dbb7
Julien Cortial <jcortial@stanford.edu>
2011-01-18 19:15:36 -0800

    Added simple node/dof buffer implementation

---
+++ Rom.d/NodeDof6Buffer.h
@@ -0,0 +1,8 @@
+#ifndef ROM_NODEDOF6BUFFER_H
+#define ROM_NODEDOF6BUFFER_H
+
+#include <OOPita.d/SimpleBuffer.h>
+
+typedef ::Pita::SimpleBuffer<double[6]> NodeDof6Buffer;
+
+#endif /* ROM_NODEDOF6BUFFER_H */

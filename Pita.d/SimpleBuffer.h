4ccdafdcf7f1b5644514f49a718dad2cb59e96e8
Julien Cortial <jcortial@stanford.edu>
2011-01-31 20:50:50 -0800

    Removed redundant and obsolete definition of SimpleBuffer

--- Pita.d/SimpleBuffer.h
+++ Pita.d/SimpleBuffer.h
@@ -1,58 +1,8 @@
-#include <cstdlib>
+#ifndef PITA_SIMPLEBUFFER_FIX_H
+#define PITA_SIMPLEBUFFER_FIX_H
 
-template <typename Scalar>
-class SimpleBuffer
-{
-  public:
-  typedef Scalar DataType;
-  
-  SimpleBuffer();
-  ~SimpleBuffer();
+#include <OOPita.d/SimpleBuffer.h>
 
-  Scalar * array() { return values_; }
-  const Scalar * array() const { return values_; }
-  const Scalar & operator[](size_t n) const { return values_[n]; }
-  Scalar & operator[](size_t n) { return values_[n]; }
-  size_t size() const { return numValues_; }
-  void size(size_t numValues);
-  void empty();
+using ::Pita::SimpleBuffer;
 
-  private:
-  static const size_t sizeOfScalar_ = sizeof(Scalar);
-
-  Scalar * values_;
-  size_t numValues_;
-};
-
-template <typename Scalar>
-SimpleBuffer<Scalar>::SimpleBuffer() : values_(NULL), numValues_(0) {}
-
-template <typename Scalar>
-SimpleBuffer<Scalar>::~SimpleBuffer()
-{
-  std::free(values_);
-}
-
-template <typename Scalar>
-void SimpleBuffer<Scalar>::size(size_t numValues)
-{
-  if (numValues == numValues_)
-    return;
-  if (numValues > 0)
-  {
-    numValues_ = numValues;
-    values_ = (Scalar*) std::realloc(values_, numValues * sizeOfScalar_);
-  }
-  else
-  {
-    empty();
-  }
-}
-
-template <typename Scalar>
-void SimpleBuffer<Scalar>::empty()
-{
-  std::free(values_);
-  values_ = NULL;
-  numValues_ = 0;
-}
+#endif /* PITA_SIMPLEBUFFER_FIX_H */

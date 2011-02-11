3f2a8b7f97569674a717ffcc13b928d4c756741f
Philip Avery <pavery@stanford.edu>
2011-02-11 14:13:47 -0800

    changed acme version to 2.9. also now configure with intel mkl blas/lapack on army cluster

--- su-ahpcrc.stanford.edu-config.cmake
+++ su-ahpcrc.stanford.edu-config.cmake
@@ -3,7 +3,7 @@
 SET(CMAKE_INCLUDE_PATH /lustre/home/avery/code
                        /lustre/home/avery/code/spooles
                        /lustre/home/avery/code/MUMPS_4.9.1/include
-                       /lustre/home/avery/code/acme-2.5e/search /lustre/home/avery/code/acme-2.5e/enforcement
+                       /lustre/home/avery/code/acme-2.9/search /lustre/home/avery/code/acme-2.9/enforcement
                        /lustre/home/avery/code/Zoltan/include)
 SET(CMAKE_LIBRARY_PATH /lustre/home/avery/code/arpack
                        /lustre/home/avery/code/spooles
@@ -12,18 +12,19 @@ SET(CMAKE_LIBRARY_PATH /lustre/home/avery/code/arpack
                        /lustre/home/avery/code/scalapack-1.8.0
                        /lustre/home/avery/code/MUMPS_4.9.1/lib
                        /lustre/home/avery/code/metis-4.0.1
-                       /lustre/home/avery/code/acme-2.5e/lib
+                       /lustre/home/avery/code/acme-2.9/lib
                        /lustre/home/avery/code/Zoltan/Obj_generic)
 # blas and lapack
-SET(BLAS_LIBRARIES /lustre/home/avery/code/lapack-3.2.1/blas_LINUX.a)
-SET(LAPACK_LIBRARIES /lustre/home/avery/code/lapack-3.2.1/lapack_LINUX.a ${BLAS_LIBRARIES})
+#SET(BLAS_LIBRARIES /lustre/home/avery/code/lapack-3.2.1/blas_LINUX.a)
+#SET(LAPACK_LIBRARIES /lustre/home/avery/code/lapack-3.2.1/lapack_LINUX.a ${BLAS_LIBRARIES})
+SET(LAPACK_LIBRARIES -Wl,--start-group /opt/intel/mkl/10.0.1.014/lib/em64t/libmkl_intel_lp64.so /opt/intel/mkl/10.0.1.014/lib/em64t/libmkl_sequential.so /opt/intel/mkl/10.0.1.014/lib/em64t/libmkl_lapack.so /opt/intel/mkl/10.0.1.014/lib/em64t/libmkl_core.so -Wl,--end-group CACHE STRING "Path to a library.")
 SET(LAPACK_FOUND true)
 # add anything missed by cmake auto-detection
 SET(EXTRALIB /opt/intel/fce/10.1.015/lib/libifcore.so 
              /opt/intel/fce/10.1.015/lib/libsvml.so
+             /usr/lib64/libg2c.so.0
              -lgfortran
              CACHE STRING "Extra link parameters")
 SET(EXTRALIB_MPI /usr/mpi/gcc/openmpi-1.2.6/lib64/libmpi_f77.so
                  CACHE STRING "Extra MPI link parameters")
 
-#SET(BLA_VENDOR Intel10_64lp)

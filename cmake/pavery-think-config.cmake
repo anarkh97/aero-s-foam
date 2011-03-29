5fffe42cd3cd96eda6bca916fb3eed6620c06f96
Philip Avery <pavery@stanford.edu>
2011-03-29 15:05:40 -0700

    merge

--- cmake/pavery-think-config.cmake
+++ cmake/pavery-think-config.cmake
@@ -1,6 +1,6 @@
 # use to tell cmake where to search for the include files and libraries of:
 # acme, arpack, blacs, metis, mumps, scalapack, spooles, zoltan
-SET(CMAKE_INCLUDE_PATH /home/pavery/Codes/eigen3;/home/pavery/Codes/Acme/acme-2.9/search;/home/pavery/Codes/Acme/acme-2.9/enforcement;/home/pavery/Codes/Zoltan/include)
+SET(CMAKE_INCLUDE_PATH /home/pavery/Codes/eigen;/home/pavery/Codes/Acme/acme-2.9/search;/home/pavery/Codes/Acme/acme-2.9/enforcement;/home/pavery/Codes/Zoltan/include)
 SET(CMAKE_LIBRARY_PATH /home/pavery/Codes/Acme/acme-2.9/lib;/home/pavery/Codes/Zoltan/Obj_generic;/home/pavery/Codes/GotoBLAS2/lib-single-threaded;/home/pavery/Codes/ARPACK)
 #SET(CMAKE_INCLUDE_PATH /home/pavery/Codes/Acme/acme-2.5e/search;/home/pavery/Codes/Acme/acme-2.5e/enforcement;/home/pavery/Codes/Zoltan/include)
 #SET(CMAKE_LIBRARY_PATH /home/pavery/Codes/Acme/acme-2.5e/lib;/home/pavery/Codes/Zoltan/Obj_generic;/home/pavery/Codes/GotoBLAS2/lib-single-threaded;/home/pavery/Codes/ARPACK)

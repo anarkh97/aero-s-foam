b11172bcbd2de4c9c5cdf48f1bd4947e30549ac4
Mark Potts <mpotts@hpti.com>
2011-05-05 15:56:14 -0700

    New files for regression testing

---
+++ Regression.d/buildInputs.py
@@ -0,0 +1,242 @@
+#!/usr/bin/python
+"""Tool to build input files used for regression testing of AERO codes"""
+
+__author__ = "Mark A. Potts (mpotts@hpti.com)"
+__version__ = "$Revision: 0.1 $"
+__date__ = "2011/02/14"
+
+import sys, os, re, glob
+#import argparse
+
+
+def buildInputs(params):
+  mycwd = os.getcwd()
+  if(mycwd.find("Regression.d") < 0):
+    if(os.path.exists("Regression.d")==0):
+      os.mkdir("Regression.d")  
+    os.chdir("Regression.d")  
+
+  ploc = -1
+  bpath = re.compile("\-c")
+  i = 0
+  copyInputs = 0
+
+  for s in params:
+    if(re.search(bpath,s)):
+       ploc = i
+       copyInputs = 1
+    i=i+1
+
+  if(ploc != -1):
+    basepath = params[ploc+1]
+    del params[ploc+1]
+    del params[ploc]
+
+  if(copyInputs == 1): # just copy the all the subdirectories and input files from the basepath parameter
+    import shutil
+    print "listing for " + basepath
+    listdir = os.listdir(basepath)
+    mycwd = os.getcwd()
+    for infile in listdir:
+      fullpath = os.path.join(basepath,infile)
+      if (os.path.isdir(fullpath)==True)&(infile[0] != "."):
+        if(os.path.exists(os.path.join(mycwd,infile)) != True):
+          print "creating directory : " + infile
+          newdir = os.mkdir(infile)
+        
+        print fullpath, os.path.exists(fullpath) 
+        origfiles = fullpath + "/*.inp"
+        print origfiles, infile
+        command = "cp "+ origfiles+ " "+infile 
+        os.system(command)
+        origfiles = fullpath + "/*.include"
+        command = "cp "+ origfiles+ " "+infile 
+        os.system(command)
+        origfiles = fullpath + "/run.*"
+        command = "cp "+ origfiles+ " "+infile 
+        os.system(command)
+        origfiles = fullpath + "/scp.*"
+        command = "cp "+ origfiles+ " "+infile 
+        os.system(command)
+  else:
+    if(params[1] == 'ALL'):
+      PROBLEM_NAMES=['statics','nlstatics','eigen','freqsweep','dynamics','nldynamics','impe','tempstatics','tempdynamics','tempnldynamics','tempnlstatics']
+    else:
+      PROBLEM_NAMES = [params[1]]
+
+    for problem_type in PROBLEM_NAMES:
+  
+      if(os.path.exists(problem_type)==0):
+        os.mkdir(problem_type)  
+
+      os.chdir(problem_type)
+      dirname = os.getcwd()
+      runfilename = "run."+problem_type 
+      qsubfilename = "scp."+problem_type 
+      RUNFILE = open(runfilename,"w")
+      MPIFILE = open(qsubfilename,"w")
+      MPIFILE.write("#!/bin/bash\n#PBS -N test\n#PBS -l nodes=1:ppn=8,walltime=1:00:00\n\n")
+      MPIFILE.write("cd %s\n" % dirname)
+     
+      command = "chmod +x " + runfilename
+      os.system(command)
+      command = "cp /lustre/home/mpotts/regression/*.include ."
+      os.system(command)
+
+      OUTPUT = ["gdisplac","stressvm","strainxx","strainxy","strainxz",\
+                "strainxx","strainxy","strainxz","strainxx","strainxy", \
+                "strainxz","inxforce","inyforce","inzforce","axmoment", \
+                "aymoment","azmoment","energies","gvelocit","gacceler", \
+                "displmod","rotatmod","gdispmod"]
+
+      STATICS = ["sparse","skyline","mumps","spooles","gmres","direct",\
+                 "spooles pivot","mumps pivot",\
+                 "frontal","pcg","bcg","cr","FETI",\
+                 "FETI 1","FETI 2 OLD","FETI 2 NEW","FETI DP","FETI DPH"]
+
+      INCLUDE = ["\"mesh.include\""]
+
+      DYNAMICS = ["time\t0.0\t3.0e-5\t3.0e-2",\
+                  "time\t0.0\t1.0e-4\t3.0e-2",\
+                  "time\t0.0\t3.0e-4\t3.0e-2"]
+
+      IMPE =      ["freq 10.0\ndamp 1e-6 1.0",\
+                   "freq 10.0\ndamp 1e-7 1.0",\
+                   "freq 10.0\ndamp 1e-5 1.0"]
+
+      NONLINEAR = ["maxitr 10\nnltol 1.0e-6\nrebuild 1",\
+                   "maxitr 10\nnltol 1.0e-7\nrebuild 1",\
+                   "maxitr 10\nnltol 1.0e-5\nrebuild 1"]
+
+      EIGEN = ["arpack\nnsbspv 20\nneigpa 12\ntoleig 1.0e-10\ntoljac 1.0e-05",\
+               "arpack\nnsbspv 20\nneigpa 12\ntoleig 1.0e-10\ntoljac 1.0e-04",\
+               "arpack\nnsbspv 20\nneigpa 12\ntoleig 1.0e-10\ntoljac 1.0e-6"]
+
+      SHIFT = ["0","10","100","1000"]
+
+      if(problem_type == "tempnlstatics"):
+        EXTRAS = ["*","*","*","*","*id A   E   nu  rho    c   k     h   P   Ta  q     w   etc...","*"]
+        INCLUDE = ["\"mesh_temp.include\""]
+        OUTPUT = ["gtempera","gtempvel","heatflxx","heatflxy","heatflxz","grdtempx","grdtempy","grdtempz"]
+        MATERIALS = ["1   0.0 0.0 0.0 0.0 0.0 202.4 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0"]
+        NAMELIST = ["STATICS\n","NONLINEAR\n","OUTPUT\n","MATERIALS\n","INCLUDE "]
+        OPTIONSLIST = [STATICS,NONLINEAR,OUTPUT,MATERIALS,INCLUDE]
+
+      if(problem_type == "tempnldynamics"):
+        DYNAMICS = ["heat\t0.5\ntime\t1.0\t1.0\t200.0"]
+        EXTRAS = ["*","newmark","*","*","*","*","*id A   E   nu  rho    c   k     h   P   Ta  q     w   etc...","*"]
+        INCLUDE = ["\"mesh_temp.include\""]
+        OUTPUT = ["gtempera","gtempvel","heatflxx","heatflxy","heatflxz","grdtempx","grdtempy","grdtempz"]
+        MATERIALS = ["1   0.0 0.0 0.0 2719.0  0.0 202.4 0.0 0.0 0.0 871.0   0.0 0.0 0.0 0.0"]
+        NAMELIST = ["STATICS\n","DYNAMICS\n","NONLINEAR\n","OUTPUT\n","MATERIALS\n","INCLUDE "]
+        OPTIONSLIST = [STATICS,DYNAMICS,NONLINEAR,OUTPUT,MATERIALS,INCLUDE]
+
+      if(problem_type == "tempdynamics"):
+        DYNAMICS = ["mech\t0.25\t0.5",\
+                    "time\t1.0\t1.0\t200.0"]
+        EXTRAS = ["*","newmark","*","*id A   E   nu  rho    c   k     h   P   Ta  q     w   etc...","*","*"]
+        INCLUDE = ["\"mesh_temp.include\""]
+        OUTPUT = ["gtempera","gtempvel","heatflxx","heatflxy","heatflxz","grdtempx","grdtempy","grdtempz"]
+        MATERIALS = ["1   0.0 0.0 0.0 2719.0  0.0 202.4 0.0 0.0 0.0 871.0   0.0 0.0 0.0 0.0"]
+        NAMELIST = ["STATICS\n","DYNAMICS","OUTPUT\n","MATERIALS\n","INCLUDE "]
+        OPTIONSLIST = [STATICS,DYNAMICS,OUTPUT,MATERIALS,INCLUDE]
+
+      if(problem_type == "tempstatics"):
+        NAMELIST = ["STATICS\n","OUTPUT\n","MATERIALS\n","INCLUDE "]
+        EXTRAS = ["*","*","*","*id A   E   nu  rho    c   k     h   P   Ta  q     w   etc..."]
+        INCLUDE = ["\"mesh_temp.include\""]
+        OUTPUT = ["gtempera","gtempvel","heatflxx","heatflxy","heatflxz","grdtempx","grdtempy","grdtempz"]
+        MATERIALS = ["1   0.0 0.0 0.0 0.0    0.0 202.4 0.0 0.0 0.0 0.0   0.0 0.0 0.0 0.0"]
+        OPTIONSLIST=[STATICS,OUTPUT,MATERIALS,INCLUDE]
+
+      if(problem_type == "dynamics"):
+        EXTRAS = ["*","newmark\nmech\t0.25000\t0.5000\n*\ttime step\ttotal time","*","*","*"]
+        NAMELIST = ["STATICS\n","DYNAMICS\n","OUTPUT\n","INCLUDE "]
+        OPTIONSLIST = [STATICS,DYNAMICS,OUTPUT,INCLUDE]
+  
+      if(problem_type == "nldynamics"):
+        EXTRAS = ["*","newmark\nmech\t0.25000\t0.5000\n*\ttime step\ttotal time","*","*","*","*","*"]
+        NAMELIST = ["STATICS\n","DYNAMICS\n","NONLINEAR\n","OUTPUT\n","INCLUDE "]
+        OPTIONSLIST = [STATICS,DYNAMICS,NONLINEAR,OUTPUT,INCLUDE]
+
+      if(problem_type == "impe"):
+        NAMELIST = ["IMPE\n","STATICS\n","OUTPUT\n","INCLUDE "]
+        OPTIONSLIST = [IMPE,STATICS,OUTPUT,INCLUDE]
+        EXTRAS = ["*","*","*","*","*"]
+  
+      if(problem_type == "freqsweep"):
+        NAMELIST = ["STATICS\n","IMPE\n","OUTPUT\n","INCLUDE "]
+        OPTIONSLIST = [STATICS,IMPE,OUTPUT,INCLUDE]
+        EXTRAS = ["*","freqsweep 1. 3. 3 10\nrecons pade 2 9 10","*","*","*"]
+
+      if(problem_type == "nlstatics"):
+        EXTRAS = ["include \"feti.include\"\n*","*","*","*","*"]
+  
+        NAMELIST = ["STATICS\n","NONLINEAR\n","OUTPUT\n","INCLUDE "]
+        OPTIONSLIST = [STATICS,NONLINEAR,OUTPUT,INCLUDE]
+      if(problem_type == "statics"):
+        NAMELIST = ["STATICS\n","OUTPUT\n","INCLUDE "]
+        EXTRAS = ["*","*","*"]
+        OPTIONSLIST=[STATICS,OUTPUT,INCLUDE]
+
+      if(problem_type == "eigen"):
+        EXTRAS = ["*","*","*","*","*","*"]
+        NAMELIST = ["STATICS\n","EIGEN\n","SHIFT ","OUTPUT\n","INCLUDE "]
+        OPTIONSLIST=[STATICS,EIGEN,SHIFT,OUTPUT,INCLUDE]
+  
+      options = 0
+      for i in range(len(OPTIONSLIST)):
+        if(len(OPTIONSLIST[i]) >  options):
+          options = len(OPTIONSLIST[i])
+      for i in range(options):
+        idname = problem_type
+        for j in range(len(OPTIONSLIST)-1):
+          if((NAMELIST[j] != "")&(OPTIONSLIST[j][i % len(OPTIONSLIST[j])].find(" ") != 1)&
+                  (OPTIONSLIST[j][i % len(OPTIONSLIST[j])].find("-") == -1 )&
+                  (OPTIONSLIST[j][i % len(OPTIONSLIST[j])].find("\t") == -1 )):
+            idname = idname + "_" + OPTIONSLIST[j][i % len(OPTIONSLIST[j])]
+
+        idname = idname.replace(" ","_",10)
+  
+        OUTPUT_FILENAME = idname+".dat" + " 1"
+   
+        filename = idname+".inp"
+        FILE = open(filename,"w")
+        FILE.write("CONTROL\n");
+        FILE.write(idname);
+        if(idname.find("temp") != -1):
+          FILE.write("\n2\n\"nodeset\"\n\"elemset\"\n*\n");
+        else:
+          FILE.write("\n1\n\"nodeset\"\n\"elemset\"\n*\n");
+        for j in range(len(OPTIONSLIST)):
+          FILE.write(NAMELIST[j])
+          FILE.write(OPTIONSLIST[j][i  % len(OPTIONSLIST[j])])
+          if(NAMELIST[j].find("OUTPUT") != -1 ):
+            FILE.write(" %s" % OUTPUT_FILENAME);
+          FILE.write("\n")
+          if(NAMELIST[j].find("FETI") != -1):
+            FILE.write("include \"feti.include\"\n*")
+          else:
+            FILE.write(EXTRAS[j])
+          FILE.write("\n")
+        FILE.write("END\n")
+        FILE.close()
+        command = "aeros "
+        re.compile("FETI");
+        if(idname.find("FETI") != -1 ):
+          command = command + "-n 2 --dec "
+        print "Creating %s" % filename
+        MPIFILE.write("echo mpirun -n 4 %s %s\n" % (command,filename.replace(" ","_")))
+        MPIFILE.write("mpirun -n 4 %s %s\n" % (command,filename.replace(" ","_")))
+        RUNFILE.write("echo %s %s\n" % (command,filename.replace(" ","_")))
+        RUNFILE.write("%s %s\n" % (command,filename.replace(" ","_")))
+      os.chdir('../')
+
+if __name__ == "__main__":
+
+# parser = argparse.ArgumentParser(description='Build Regression Testing Inputs.')
+# parser.add_argument('--c')
+# basepath = parser.parse_args(sys.argv.split())
+
+  params = sys.argv
+  buildInputs(params)
---
+++ Regression.d/dataComp.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python
+"""Tool designed to compare data files produced by regression testing and report discrepancies"""
+
+__author__ = "Mark A. Potts (mpotts@hpti.com)"
+__version__ = "$Revision: 0.1 $"
+__date__ = "2011/02/17"
+
+import sys, os, re, md5, subprocess, math, glob, datetime, time
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
+
+def directComp(basefile,file,SUMMARY_FILE):
+  COMP = list(open(file).read().splitlines())
+  BASE = list(open(basefile,"r").read().splitlines())
+
+  MaxDiff = 0.0
+  MaxDiffLine = -1
+  MaxDiffLoc = -1
+  MaxDiffVals = (-1,-1)
+  if(len(BASE) != len(COMP)):
+    print bcolors.WARNING + "WARNING-- %s and %s have different lengths!" % (file,basefile) + bcolors.ENDC
+    print len(BASE)
+    print len(COMP)
+    outstring = "\tBaseline file length = %d, New file length = %d\n" %(len(BASE),len(COMP))
+    SUMMARY_FILE.write(outstring)
+    return  
+   
+  for i in range(len(BASE)):
+    BASE[i].replace(","," ");
+    COMP[i].replace(","," ");
+    BASE[i].strip()
+    COMP[i].strip()
+    basewords = BASE[i].split()
+    compwords = COMP[i].split()
+    if(len(basewords) != len(compwords)):
+      print "WARNING--Different line lengths at line %d of %f!!" %(i,file)
+    else:
+      for j in range(len(basewords)):
+        if(not(basewords[j].isalpha())and not(compwords[j].isalpha())):
+          diff = float(basewords[j]) - float(compwords[j])
+          if(math.fabs(diff) > MaxDiff):
+            MaxDiff = math.fabs(diff)
+            MaxDiffLine = i
+            MaxDiffLoc = j
+            MaxDiffVals = (basewords[j],compwords[j])
+
+
+  print bcolors.OKBLUE + "\t\t\t\tMax diff is %e on line %d at location %d" %(MaxDiff,MaxDiffLine,MaxDiffLoc) + bcolors.ENDC
+  outstring = "\tMax diff is %e on line %d at location %d\n" %(MaxDiff,MaxDiffLine,MaxDiffLoc)
+  SUMMARY_FILE.write(outstring)
+
+  print bcolors.OKBLUE + "\t\t\t\tvals were %s " % (MaxDiffVals,) + bcolors.ENDC
+  outstring = "\tvals were %s \n" % (MaxDiffVals,)
+  SUMMARY_FILE.write(outstring)
+
+def dComp(params):
+  import time, glob
+  import subprocess
+
+  summary_filename = "reg_test_summary"
+  mycwd = os.getcwd()
+  if(mycwd.find("Regression.d") == -1):
+    os.chdir("Regression.d")
+  if(os.path.exists(summary_filename)):
+    last_mod = os.path.getmtime(summary_filename)
+    time_now = time.mktime(time.localtime())
+    print "time now = %d and last_mod = %d, diff = %d \n" % (time_now,last_mod,time_now - last_mod)
+    if((time_now - last_mod) > 300):
+      SUMMARY_FILE = open(summary_filename,"w")
+    else:
+      SUMMARY_FILE = open(summary_filename,"a")
+  else:
+    SUMMARY_FILE = open(summary_filename,"w")
+  outstring = "\n\nSummary of regression test run as:\n%s\n" % params
+  SUMMARY_FILE.write(outstring)
+  now = datetime.datetime.now()
+  time_now = now.time()
+  date_now = now.date()
+  outstring = "\n\nTest started at %s on %s\n"% (time_now,date_now)
+  SUMMARY_FILE.write(outstring)
+  exactMatches = 0
+  Total = 0
+  loc = -1
+  sloc = -1
+  rloc = -1
+  i = 0 
+  pattern = re.compile("\-r")
+  runLocal = re.compile("\-l")
+  sendMail = re.compile("\-s")
+
+  for s in params:
+    if(re.search(pattern,s)):
+       loc = i
+    if(re.search(sendMail,s)):
+       sloc = i
+    if(re.search(runLocal,s)):
+       rloc = i
+    i=i+1
+
+  if(sloc != -1):
+    sendMail = 1
+    del params[sloc]
+  else:
+     sendMail = 0
+ 
+  if(loc != -1):
+    run = 1
+    del params[loc]
+  else:
+    run = 0 
+
+  if(rloc != -1):
+    lrun = 1
+    del params[rloc]
+  else:
+    lrun = 0 
+
+  files = [] 
+  if(params[1] == 'ALL'):
+    del params[0:1]
+    
+    PROBLEM_NAMES=['statics','nlstatics','eigen','dynamics','nldynamics',\
+                   'freqsweep','impe','tempstatics','tempnlstatics',\
+                   'tempdynamics','tempnldynamics']
+    for names in PROBLEM_NAMES:
+      indir = names+"/"
+      os.chdir(indir)
+      if(run == 1):
+        qsubScript = "scp."+names
+        retcode = subprocess.Popen(["qsub", qsubScript ], stdout=subprocess.PIPE).communicate()[0]
+        qsubRetfile = "test.o"+numbers
+        while True:
+          if(os.path.exists(qsubRetfile)):
+            break
+          time.sleep(10)
+      if(lrun == 1):
+        command = "run."+names+" >& /lustre/home/mpotts/reg.out"
+        os.system(command)
+      os.chdir('../') 
+      for infile in glob.glob( os.path.join(indir, '*.dat') ):
+        files.append(infile)
+  else: 
+    del params[0]
+    indirs = params
+    for indir in indirs:
+      indirp = indir+"/"
+      print "%s" % indir
+      for infile in glob.glob( os.path.join(indirp, '*.dat') ):
+        files.append(infile)
+      os.chdir(indir)
+      if(run == 1):
+        qsubScript = "scp."+indir
+        retcode = subprocess.Popen(["qsub", qsubScript ], stdout=subprocess.PIPE).communicate()[0]
+        numbers = filter(lambda x: x.isdigit(), retcode)
+        qsubRetfile = "test.o"+numbers
+        while True:
+          if(os.path.exists(qsubRetfile)):
+            break
+          time.sleep(10)
+      if(lrun == 1):
+        command = "run."+indir+" >& /lustre/home/mpotts/reg.out"
+        os.system(command)
+      os.chdir('../') 
+
+  result = 0
+  
+  for file in files:
+     basefile = "/lustre/home/mpotts/regression/baseline/"+file
+     p = subprocess.Popen("md5sum " + file, shell = True, stdout=subprocess.PIPE) # don't forget to "import subprocess"
+     newmd5 = p.stdout.readline().split() # now the md5 variable contains the MD5 sum
+     p.wait() # some clean up
+     p = subprocess.Popen("md5sum " + basefile, shell = True, stdout=subprocess.PIPE) # don't forget to "import subprocess"
+     oldmd5 = p.stdout.readline().split() # now the md5 variable contains the MD5 sum
+     p.wait() # some clean up
+     Total += 1
+     if(newmd5[0] == oldmd5[0]):
+       print bcolors.OKGREEN + " \tExact match "+ bcolors.ENDC, file
+       exactMatches += 1
+       outstring = "\texact match " + file + "\n"
+       SUMMARY_FILE.write(outstring)
+     else:
+       print bcolors.FAIL + " \tDiscrepancy " + bcolors.ENDC, file
+       outstring = "\tDiscrepancy " + file + "\n"
+       SUMMARY_FILE.write(outstring)
+       directComp(basefile,file,SUMMARY_FILE)
+       result = 1
+  outstring = "%d exact matches found out of %d total cases\n" %(exactMatches,Total)
+  SUMMARY_FILE.write(outstring)
+  now = datetime.datetime.now()
+  time = now.time()
+  date = now.date()
+  outstring = "Test completed at %s on %s\n"% (time,date)
+  SUMMARY_FILE.write(outstring)
+
+  if(sendMail == 1):
+    mail_file = open(".mail_command","w")
+    command = "mail -s \"Regression Test Summary\" mpotts@hpti.com < reg_test_summary"
+    mail_file.write(command)
+    print(command)
+    os.system(command)
+
+  sys.exit(result)
+
+if __name__ == "__main__":
+  params = sys.argv
+  dComp(params)
+
+

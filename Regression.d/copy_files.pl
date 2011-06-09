96eca7bb663a21f048cc1a49584c400dea8913b7
Mark Potts <mpotts@hpti.com>
2011-06-09 13:33:47 -0700

    Added case for in source builds

--- Regression.d/copy_files.pl
+++ Regression.d/copy_files.pl
@@ -2,11 +2,12 @@
 
   $src = @ARGV[0];
   $dest = @ARGV[1];
-  $command = "cp $src/\*.include $dest";
-  printf "$command \n";
-  system "$command";
-  $command = "cp $src/\*.py $dest";
-  system "$command";
-  $command = "ln -s $src/baseline $dest";
-  system "$command";
+  if($src != $dest) {
+    $command = "cp $src/\*.include $dest";
+    system "$command";
+    $command = "cp $src/\*.py $dest";
+    system "$command";
+    $command = "ln -s $src/baseline $dest";
+    system "$command";
+  }  
 

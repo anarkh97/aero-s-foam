1eea08275df457b9350b6b39d8ed8f2b911f3b3d
Julien Cortial <jcortial@stanford.edu>
2011-08-10 13:47:42 -0700

    Fixed integer overflow so that large binary files are correctly handled

--- Utils.d/BinFileHandler.h
+++ Utils.d/BinFileHandler.h
@@ -71,8 +71,8 @@ template <class Scalar>
 void BinFileHandler::read(Scalar *p, int nobjs)
 {
 
-  if (file) int ret = fread(p, sizeof(Scalar), nobjs, file);
-  else int ret = ::read(fileid, p, nobjs*sizeof(Scalar));
+  if (file) fread(p, sizeof(Scalar), nobjs, file);
+  else ::read(fileid, p, nobjs*sizeof(Scalar));
 
   cpos += nobjs*sizeof(Scalar);
 
@@ -89,7 +89,7 @@ void BinFileHandler::write(Scalar *p, int nobjs)
   if (swapBytes) swapVector(p, nobjs);
 
   if (file) fwrite(p, sizeof(Scalar), nobjs, file);
-  else int ret = ::write(fileid, p, nobjs*sizeof(Scalar)); 
+  else ::write(fileid, p, nobjs*sizeof(Scalar)); 
 
   cpos += nobjs*sizeof(Scalar);
 
@@ -140,7 +140,7 @@ inline
 BinFileHandler::OffType BinFileHandler::tell() 
 { 
 
-  int pos;
+  OffType pos;
 
   if (file) 
     pos = ftell(file);

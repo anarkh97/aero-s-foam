61257e4c884e3478ac6fb742bd1874133bbee99f
Philip Avery <pavery@stanford.edu>
2011-11-30 11:39:17 -0800

    added acme license info

--- Acme.d/search/ContactTriFaceL3.C
+++ Acme.d/search/ContactTriFaceL3.C
@@ -1,4 +1,22 @@
-// $Id$
+// This file is part of a modified version of ACME: Algorithms for Contact in
+// a Multiphysics Environment, derived from version 2.7f
+//
+// Copyright (C) 2007 Sandia Corporation
+// Copyright (C) 2011 Stanford University
+//
+// ACME is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// ACME is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with ACME.  If not, see <http://www.gnu.org/licenses/>.
+
 
 #include <algorithm>
 
@@ -956,7 +974,6 @@ int ContactTriFaceL3::FaceEdge_Intersection(VariableHandle POSITION,
   POSTCONDITION( 0 );
   return intersection;
 #else
-  std::cout << "KHP: ContactTriFaceL3::FaceEdge_Intersection\n";
   // Do bounding box check
   int i, j;
   Real edge_min[3], edge_max[3];

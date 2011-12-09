1b8810e75fe0964c7beef37a45706e34a71cab11
Julien Cortial <jcortial@stanford.edu>
2011-12-09 14:12:43 -0800

    Added proper timestamp to snapshot collection

--- Rom.d/SnapshotNonLinDynamic.h
+++ Rom.d/SnapshotNonLinDynamic.h
@@ -34,9 +34,11 @@ protected:
   // Interface to implementation
   class Impl {
   public:
+    virtual void lastMidTimeIs(double) = 0;
+    virtual void lastDeltaIs(double) = 0;
     virtual void stateSnapshotAdd(const GeomState &) = 0;
     virtual void postProcess() = 0;
-    
+ 
     virtual ~Impl() {}
 
   protected:
@@ -50,6 +52,8 @@ protected:
 
 private:
   // Snapshot collection 
+  void saveMidTime(double t) { impl_->lastMidTimeIs(t); }
+  void saveDelta(double dt) { impl_->lastDeltaIs(dt); }
   void saveStateSnapshot(const GeomState &state) { impl_->stateSnapshotAdd(state); }
  
   BasisType outputBasisType_;
@@ -61,16 +65,28 @@ private:
 // Provides hooks to be used in NLDynamSolver to call the snapshot collection functions
 class SnapshotNonLinDynamic::Updater : public IncrUpdater<SnapshotNonLinDynamic, GenVector<double>, GeomState> {
 public:
+  static double integrate(SnapshotNonLinDynamic *pbd, int *refState, GeomState *geomState,
+                          GenVector<double> *du, GenVector<double> &residual,
+                          GenVector<double> &elementInternalForce, GenVector<double> &gRes, GenVector<double> &vel_n,
+                          GenVector<double> &accel, double midTime) {
+    pbd->saveMidTime(midTime);
+
+    return IncrUpdater<SnapshotNonLinDynamic, GenVector<double>, GeomState>::integrate(
+        pbd, refState, geomState, du, residual, elementInternalForce, gRes, vel_n, accel, midTime);
+  }
+
   static void midpointIntegrate(SnapshotNonLinDynamic *pbd, GenVector<double> &velN,
                                 double delta, GeomState *refState, 
                                 GeomState *geomState, GenVector<double> *dummy1,
                                 GenVector<double> &dummy2, GenVector<double> &dummy3,
                                 GenVector<double> &dummy4, GenVector<double> &acceleration) {
-    pbd->saveStateSnapshot(*geomState);
+    pbd->saveDelta(delta); 
 
     IncrUpdater<SnapshotNonLinDynamic, GenVector<double>, GeomState>::midpointIntegrate(
         pbd, velN, delta, refState, geomState,
         dummy1, dummy2, dummy3, dummy4, acceleration);
+    
+    pbd->saveStateSnapshot(*geomState);
   }
 };
 

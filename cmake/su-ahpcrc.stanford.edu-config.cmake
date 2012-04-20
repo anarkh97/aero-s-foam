4e61458b440687323c9c4ddb06c1e8ef29d181c0
Philip Avery <pavery@stanford.edu>
2012-04-19 23:44:04 -0700

    fixed issue with FindBoost module and cmake on ahpcrcfe

--- cmake/su-ahpcrc.stanford.edu-config.cmake
+++ cmake/su-ahpcrc.stanford.edu-config.cmake
@@ -1,27 +1,20 @@
 # use to tell cmake where to search for the include files and libraries of:
 # acme, arpack, blacs, metis, mumps, scalapack, spooles, zoltan
-SET(CMAKE_INCLUDE_PATH /lustre/home/avery/code
-                       /lustre/home/avery/code/spooles
-                       /lustre/home/avery/code/MUMPS_4.9.1/include
-                       /lustre/home/avery/code/Zoltan
-                       /lustre/home/avery/code/Zoltan/include)
-SET(CMAKE_LIBRARY_PATH /lustre/home/avery/code/arpack
-                       /lustre/home/avery/code/spooles
-                       /lustre/home/avery/code/spooles/MT/src
-                       /lustre/home/avery/code/BLACS/LIB
-                       /lustre/home/avery/code/scalapack-1.8.0
-                       /lustre/home/avery/code/MUMPS_4.9.1/lib
-                       /lustre/home/avery/code/metis-4.0.1
-                       /lustre/home/avery/code/Zoltan/Obj_generic)
-# blas and lapack
-#SET(BLAS_LIBRARIES /lustre/home/avery/code/lapack-3.2.1/blas_LINUX.a)
-#SET(LAPACK_LIBRARIES /lustre/home/avery/code/lapack-3.2.1/lapack_LINUX.a ${BLAS_LIBRARIES})
-## having problems with mkl (try again when newer version is installed)
-#SET(LAPACK_LIBRARIES /opt/intel/mkl/10.0.1.014/lib/em64t/libmkl_lapack.so /opt/intel/mkl/10.0.1.014/lib/em64t/libmkl_intel_lp64.so /opt/intel/mkl/10.0.1.014/lib/em64t/libmkl_sequential.so /opt/intel/mkl/10.0.1.014/lib/em64t/libmkl_core.so /opt/intel/mkl/10.0.1.014/lib/em64t/libiomp5.so -lpthread CACHE STRING "Path to a library.")
-#SET(LAPACK_FOUND true)
+# note: for openmpi 1.2.6 + gcc 4.1 use libs built in ~avery/code.copy
+SET(CMAKE_INCLUDE_PATH 
+    /lustre/home/avery/code
+    /lustre/home/avery/code/spooles
+    /lustre/home/avery/code/MUMPS_4.10.0/include
+    /lustre/home/avery/code/trilinos/trilinos-10.8.4-Obj_cmake/include
+    /lustre/home/avery/code/trilinos/trilinos-10.8.4-Source/packages/zoltan/src
+)
+SET(CMAKE_LIBRARY_PATH 
+    /lustre/home/avery/code/arpack
+    /lustre/home/avery/code/spooles
+    /lustre/home/avery/code/spooles/MT/src
+    /lustre/home/avery/code/BLACS/LIB
+    /lustre/home/avery/code/scalapack-1.8.0
+    /lustre/home/avery/code/MUMPS_4.10.0/lib
+    /lustre/home/avery/code/trilinos/trilinos-10.8.4-Obj_cmake/lib
+)
 
-# add anything missed by cmake auto-detection
-SET(EXTRALIB -lgfortran 
-             CACHE STRING "Extra link parameters")
-SET(EXTRALIB_MPI /usr/mpi/gcc/openmpi-1.2.6/lib64/libmpi_f77.so
-                 CACHE STRING "Extra MPI link parameters")

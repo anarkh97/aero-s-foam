356b365cb2829f2c9ff4f7c3bf4df159ab92917c
Philip Avery <pavery@stanford.edu>
2012-09-14 12:15:06 -0700

    fixed memory leak in ElementSamplingDriver

--- Rom.d/ElementSamplingDriver.C
+++ Rom.d/ElementSamplingDriver.C
@@ -127,19 +127,19 @@ ElementSamplingDriver::assembleTrainingData(const VecBasis &snapshots, DblFwdIt 
   Vector podComponents(podVectorCount), elemTarget(podVectorCount);
   SimpleBuffer<double> elementForce(domain_->maxNumDOF());
 
-  DblFwdIt timeStampIt = timeStampFirst;
-
   // Project snapshots on POD basis to get training configurations
   for (int iSnap = 0; iSnap != snapshotCount; ++iSnap) {
     expand(podBasis, reduce(podBasis, snapshots[iSnap], podComponents), displac[iSnap]);
   }
 
   for (int iElem = 0; iElem != elementCount(); ++iElem) {
-    
+
+    DblFwdIt timeStampIt = timeStampFirst;
+    int *nodes = domain_->getElementSet()[iElem]->nodes();
     for (int iSnap = 0; iSnap != snapshotCount; ++iSnap) {
       //geomState_->explicitUpdate(domain_->getNodes(), displac[iSnap]);
       geomState_->explicitUpdate(domain_->getNodes(), domain_->getElementSet()[iElem]->numNodes(),
-                                 domain_->getElementSet()[iElem]->nodes(), displac[iSnap]); // just update the nodes of element iElem
+                                 nodes, displac[iSnap]); // just update the nodes of element iElem
       // Evaluate and store element contribution at training configuration
       domain_->getElemInternalForce(*geomState_, *timeStampIt, NULL, *(corotators_[iElem]), elementForce.array(), kelArray_[iElem]);
       elemTarget.zero();
@@ -160,6 +160,7 @@ ElementSamplingDriver::assembleTrainingData(const VecBasis &snapshots, DblFwdIt 
         trainingTarget[podVectorCount * iSnap + iPod] += elemTarget[iPod];
       }
     }
+    delete [] nodes;
     timeStampIt++;
   }
 }

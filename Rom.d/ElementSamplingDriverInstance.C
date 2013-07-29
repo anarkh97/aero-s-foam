0df919ff5d3c52ede60b57f4745466163ad56fee
Philip Avery <pavery@stanford.edu>
2013-07-29 15:08:23 -0700

    fixed compilation error

--- Rom.d/ElementSamplingDriverInstance.C
+++ Rom.d/ElementSamplingDriverInstance.C
@@ -90,7 +90,7 @@ ElementSamplingDriver<stxxl::VECTOR_GENERATOR<double,16,32,8388608,stxxl::RC,stx
 template
 void
 ElementSamplingDriver<stxxl::VECTOR_GENERATOR<double,16,32,8388608,stxxl::RC,stxxl::random>::result,stxxl::uint64>
-::postProcess(Vector &solution, bool firstTime, bool verboseFlag);
+::postProcess(Vector &solution, bool verboseFlag);
 
 template
 void

450df80a30af164bf7bb757802c9b706b4078ebc
Philip Avery <pavery@stanford.edu>
2013-12-04 14:03:07 +0100

    added hand-coded jacobian implementation for nonlinear inertial force in ROM

--- Element.d/Function.d/InertialForce.d/InertialType1ForceFunction.C
+++ Element.d/Function.d/InertialForce.d/InertialType1ForceFunction.C
@@ -1,13 +1,13 @@
 #ifdef USE_EIGEN3
-#include <Element.d/Function.d/InertialForce.d/InertialForceFunction.h>
-#include <Element.d/Function.d/Rotation.d/IncrementalRotationVectorType1.h>
+#include <Element.d/Function.d/InertialForce.d/InertialType1ForceFunction.h>
+#include <Element.d/Function.d/Rotation.d/IncrementalRotationVector.h>
 #include <iostream>
 
 namespace Simo {
 
 template<>
 Eigen::Matrix<double,3,3>
-Jacobian<double,InertialForceFunction>
+Jacobian<double,InertialType1ForceFunction>
 ::operator() (const Eigen::Matrix<double,3,1>& q, double t)
 {
   Eigen::Matrix3d J, R_n, Rref, Fx, Vx, Px;
@@ -23,12 +23,20 @@ Jacobian<double,InertialForceFunction>
   const double &alphaf = sconst[35];
   const double &alpham = sconst[36];
   const double &dt     = sconst[37];
+  const double &alphaDamp = sconst[38]; // XXX
 
-  double c1 = gamma/(dt*beta);
-  double c2 = (1-alpham)/(dt*dt*beta*(1-alphaf));
-  mat_to_vec<double>(R_n.transpose()*Rref, Psi); // assuming here that q = 0: Psi = mat_to_vec(R_n^T*exp(q)*Rref);
-  V = c1*Psi + (1-(1-alphaf)*gamma/beta)*V_n + dt*(1-alphaf)*(2*beta-gamma)/(2*beta)*A_n;
-  A = c2*Psi - (1-alpham)/(dt*beta)*V_n + ((alpham-1)/(2*beta)+1)*A_n;
+  double s1 = gamma/(dt*beta);
+  double s2 = (1-alpham)/(dt*dt*beta*(1-alphaf));
+  if( (q.array() == 0).all() ) {
+    mat_to_vec<double>(R_n.transpose()*Rref, Psi);
+  }
+  else {
+    Eigen::Matrix3d R;
+    vec_to_mat(q, R);
+    mat_to_vec<double>(R_n.transpose()*R*Rref, Psi);
+  }
+  V = s1*Psi + (1-(1-alphaf)*gamma/beta)*V_n + dt*(1-alphaf)*(2*beta-gamma)/(2*beta)*A_n;
+  A = s2*Psi - (1-alpham)/(dt*beta)*V_n + ((alpham-1)/(2*beta)+1)*A_n;
   F = Rref*(J*A + V.cross(J*V));
   P = J*V;
   Fx <<     0, -F[2],  F[1],
@@ -42,10 +50,18 @@ Jacobian<double,InertialForceFunction>
         -P[1],  P[0],     0;
 
   Eigen::Array<double,18,1> sconst2 = sconst.segment<18>(15);
-  Jacobian<double,IncrementalRotationVectorType1> dPsidq(sconst2, Eigen::Array<int,0,1>::Zero());
+  Jacobian<double,IncrementalRotationVector> dPsidq(sconst2, Eigen::Array<int,0,1>::Zero());
   //std::cerr << "d[mat_to_vec(R_n^T*vec_to_mat(psi)*Rref)]/dq = \n" << dPsidq(q, 0.) << std::endl;
 
-  return -0.5*Fx + Rref*(c2*J + c1*(Vx*J - Px))*dPsidq(q,t);
+  if( (q.array() == 0).all() ) {
+    return -0.5*Fx + Rref*(s2*J + s1*(Vx*J - Px))*dPsidq(q, t);
+  }
+  else {
+    Eigen::Matrix3d T, C2;
+    tangential_transf(q, T);
+    directional_deriv2(q, F, C2);
+    return C2 + T.transpose()*Rref*(s2*J + s1*(Vx*J - Px))*dPsidq(q, t);
+  }
 }
 
 } // namespace Simo

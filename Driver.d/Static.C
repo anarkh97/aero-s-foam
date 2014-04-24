5c34176ad257774170e1cf980e258d5f15c6a34b
Philip Avery <pavery@stanford.edu>
2014-04-24 21:25:42 +0200

    Fixed '# Elements' printed to screen so it does not include any MpcElements from contact surfaces.

--- Driver.d/Static.C
+++ Driver.d/Static.C
@@ -125,7 +125,7 @@ Domain::printStatistics(bool domain_decomp)
 
   filePrint(stderr, "\n ---------- PROBLEM PARAMETERS --------");
   filePrint(stderr, "\n ... # Nodes              = %7d ...", exactNumNodes);
-  filePrint(stderr, "\n ... # Elements           = %7d ...", numele-geoSource->numMpcElem());
+  filePrint(stderr, "\n ... # Elements           = %7d ...", numele-geoSource->numMpcElem()-contactSurfElems.size());
   filePrint(stderr, "\n ... # Unconstrained dofs = %7d ...", numDofs()-numDirichlet-numComplexDirichlet);
   filePrint(stderr, "\n ... # Constrained dofs   = %7d ...", numDirichlet+numComplexDirichlet);
   filePrint(stderr, "\n ... Total # dofs         = %7d ...", numDofs());

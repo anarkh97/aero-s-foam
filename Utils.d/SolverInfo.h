65b4fd71556494caf36562e3e267ab0a93539862
Youngsoo Choi <yc344@stanford.edu>
2014-08-02 01:37:10 -0700

    oeigenvalue is implemented

--- Utils.d/SolverInfo.h
+++ Utils.d/SolverInfo.h
@@ -196,6 +196,7 @@ struct SolverInfo {
    const char* xmatrixname;
    const char* qmatrixname;
    const char* rmatrixname;
+   const char* eigenvaluename;
 
    // Sloshing problem flag
    int sloshing;
@@ -524,6 +525,7 @@ struct SolverInfo {
                   goldfarb_check = false;
                   explicitK = false;
                   qrfactorization = false;
+                  eigenvaluename = "";
                   xmatrixname = "";
                   qmatrixname = "";
                   rmatrixname = "";

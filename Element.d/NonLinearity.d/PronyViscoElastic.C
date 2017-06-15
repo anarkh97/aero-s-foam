5805a53c883f3d42be4a8650fbb06170c393bf34
nasos <nasos_i@hotmail.com>
2017-06-15 07:02:56 -0400

    Mercurial got a bit desynchronized for some reasons. Recommited changes to the Prony template.

--- Element.d/NonLinearity.d/PronyViscoElastic.C
+++ Element.d/NonLinearity.d/PronyViscoElastic.C
@@ -1,71 +1,73 @@
 #include <Utils.d/NodeSpaceArray.h>
 #include <cmath>
 
-template<typename Material>
-PronyViscoElastic<Material>::PronyViscoElastic(double p1,
+template<typename Material, class tensor_policy>
+PronyViscoElastic<Material, tensor_policy>::PronyViscoElastic(double p1,
                       double _ginf, double _g1, double _tau1, double _g2, double _tau2, double _g3, double _tau3)
  : Material(p1), ginf(_ginf), g1(_g1), tau1(_tau1), g2(_g2), tau2(_tau2), g3(_g3), tau3(_tau3) {}
 
-template<typename Material>
-PronyViscoElastic<Material>::PronyViscoElastic(double p1, double p2,
+template<typename Material, class tensor_policy>
+PronyViscoElastic<Material, tensor_policy>::PronyViscoElastic(double p1, double p2,
                       double _ginf, double _g1, double _tau1, double _g2, double _tau2, double _g3, double _tau3)
  : Material(p1,p2), ginf(_ginf), g1(_g1), tau1(_tau1), g2(_g2), tau2(_tau2), g3(_g3), tau3(_tau3) {}
 
-template<typename Material>
-PronyViscoElastic<Material>::PronyViscoElastic(double p1, double p2, double p3,
+template<typename Material, class tensor_policy>
+PronyViscoElastic<Material, tensor_policy>::PronyViscoElastic(double p1, double p2, double p3,
                       double _ginf, double _g1, double _tau1, double _g2, double _tau2, double _g3, double _tau3)
  : Material(p1,p2,p3), ginf(_ginf), g1(_g1), tau1(_tau1), g2(_g2), tau2(_tau2), g3(_g3), tau3(_tau3) {}
 
-template<typename Material>
-PronyViscoElastic<Material>::PronyViscoElastic(double p1, double p2, double p3, double p4, 
+template<typename Material, class tensor_policy>
+PronyViscoElastic<Material, tensor_policy>::PronyViscoElastic(double p1, double p2, double p3, double p4,
                       double _ginf, double _g1, double _tau1, double _g2, double _tau2, double _g3, double _tau3)
  : Material(p1,p2,p3,p4), ginf(_ginf), g1(_g1), tau1(_tau1), g2(_g2), tau2(_tau2), g3(_g3), tau3(_tau3) {}
 
-template<typename Material>
-PronyViscoElastic<Material>::PronyViscoElastic(double p1, double p2, double p3, double p4, double p5, 
+template<typename Material, class tensor_policy>
+PronyViscoElastic<Material, tensor_policy>::PronyViscoElastic(double p1, double p2, double p3, double p4, double p5,
                       double _ginf, double _g1, double _tau1, double _g2, double _tau2, double _g3, double _tau3)
  : Material(p1,p2,p3,p4,p5), ginf(_ginf), g1(_g1), tau1(_tau1), g2(_g2), tau2(_tau2), g3(_g3), tau3(_tau3) {}
 
-template<typename Material>
-PronyViscoElastic<Material>::PronyViscoElastic(double p1, double p2, double p3, double p4, double p5, double p6,
+template<typename Material, class tensor_policy>
+PronyViscoElastic<Material, tensor_policy>::PronyViscoElastic(double p1, double p2, double p3, double p4, double p5, double p6,
                       double _ginf, double _g1, double _tau1, double _g2, double _tau2, double _g3, double _tau3)
  : Material(p1,p2,p3,p4,p5,p6), ginf(_ginf), g1(_g1), tau1(_tau1), g2(_g2), tau2(_tau2), g3(_g3), tau3(_tau3) {}
 
-template<typename Material>
-PronyViscoElastic<Material>::PronyViscoElastic(double p1, double p2, double p3, double p4, double p5, double p6, double p7,
+template<typename Material, class tensor_policy>
+PronyViscoElastic<Material, tensor_policy>::PronyViscoElastic(double p1, double p2, double p3, double p4, double p5, double p6, double p7,
                       double _ginf, double _g1, double _tau1, double _g2, double _tau2, double _g3, double _tau3)
  : Material(p1,p2,p3,p4,p5,p6,p7), ginf(_ginf), g1(_g1), tau1(_tau1), g2(_g2), tau2(_tau2), g3(_g3), tau3(_tau3) {}
 
-template<typename Material>
-PronyViscoElastic<Material>::PronyViscoElastic(double p1, double p2, double p3, double p4, double p5, double p6, double p7, double p8,
+template<typename Material, class tensor_policy>
+PronyViscoElastic<Material, tensor_policy>::PronyViscoElastic(double p1, double p2, double p3, double p4, double p5, double p6, double p7, double p8,
                       double _ginf, double _g1, double _tau1, double _g2, double _tau2, double _g3, double _tau3)
  : Material(p1,p2,p3,p4,p5,p6,p7,p8), ginf(_ginf), g1(_g1), tau1(_tau1), g2(_g2), tau2(_tau2), g3(_g3), tau3(_tau3) {}
 
-template<typename Material>
+template<typename Material, class tensor_policy>
 NLMaterial *
-PronyViscoElastic<Material>::clone() const
+PronyViscoElastic<Material, tensor_policy>::clone() const
 {
-  return new PronyViscoElastic<Material>(*this);
+  return new PronyViscoElastic<Material, tensor_policy>(*this);
 }
 
-template<typename Material>
+template<typename Material, class tensor_policy>
 int
-PronyViscoElastic<Material>::getNumStates()
+PronyViscoElastic<Material, tensor_policy>::getNumStates()
 {
-  // storage order: h1 (6), h2 (6), h3 (6), sigma(tn-1) (6),
-  return 24; // store 4 Prony PK2 stresses (24 = 6*4) from previous time step
+    std::size_t s =  tensor_policy::stride;
+
+  // storage order: h1 (6), h2 (6), h3 (6), sigma(tn-1) (6), ???? should 6 change to  stride here??
+  return s * 4; // store 4 Prony PK2 stresses (24 = 6*4) from previous time step or whatever the stride indicates
 }
 
-template<typename Material>
+template<typename Material, class tensor_policy>
 void
-PronyViscoElastic<Material>::initStates(double *state)
+PronyViscoElastic<Material, tensor_policy>::initStates(double *state)
 {
   for(int i=0; i<getNumStates(); ++i) state[i] = 0;
 }
 
-template<typename Material>
+template<typename Material, class tensor_policy>
 void
-PronyViscoElastic<Material>::getStress(Tensor *_stress, Tensor &_strain, double* state, double temp)
+PronyViscoElastic<Material, tensor_policy>::getStress(Tensor *_stress, Tensor &_strain, double* state, double temp)
 {
   // compute hyperelastic response
   Material::getStress(_stress, _strain, state, temp);
@@ -73,19 +75,21 @@ PronyViscoElastic<Material>::getStress(Tensor *_stress, Tensor &_strain, double*
   // add visco-hyperelastic contribution to long-term hyperelastic response
   Tensor_d0s2_Ss12 *stress = static_cast<Tensor_d0s2_Ss12 *>(_stress); // 2nd Piola-Kirchhoff stress tensor
 
-  for(int i = 0; i < 6; i++) {
+  std::size_t s = tensor_policy::stride;
+
+  for(int i = 0; i < s; i++) {
     // use stress history stored in state container
-    (*stress)[i] = ginf*(*stress)[i] + state[i] + state[i+6] + state[i+12];
+    (*stress)[i] = ginf*(*stress)[i] + state[i] + state[i+s] + state[i+2*s];
   }
 }
 
-template<typename Material>
-void 
-PronyViscoElastic<Material>::integrate(Tensor *_stress, Tensor *_tm, Tensor &en, Tensor &enp,
+template<typename Material, class tensor_policy>
+void
+PronyViscoElastic<Material, tensor_policy>::integrate(Tensor *_stress, Tensor *_tm, Tensor &en, Tensor &enp,
                                        double *staten, double *statenp, double temp, Tensor *cache, double dt)
 {
   // _stress - constainer for 2nd P-K stress tensor that is updated by calling integrate
-  // _tm     - elasticity tensor (derivative of 2nd P-K stress tensor) 
+  // _tm     - elasticity tensor (derivative of 2nd P-K stress tensor)
   // enp     - Green-Lagrange strain tensor
   // staten  - internal variables before computing material response (get them out of here)
   // statenp - internal variables after computing material response  (put them into here)
@@ -95,13 +99,15 @@ PronyViscoElastic<Material>::integrate(Tensor *_stress, Tensor *_tm, Tensor &en,
   // compute hyperelastic response
   Material::integrate(_stress, _tm, en, enp, staten, statenp, temp, cache, dt);
 
-  Tensor_d0s4_Ss12s34 *tm  = static_cast<Tensor_d0s4_Ss12s34 *>(_tm);
-  Tensor_d0s2_Ss12 *stress = static_cast<Tensor_d0s2_Ss12 *>(_stress);
+  typename tensor_policy::d0s4_S *tm  = static_cast<typename tensor_policy::d0s4_S *>(_tm);
+  typename tensor_policy::d0s2_S *stress = static_cast<typename tensor_policy::d0s2_S *>(_stress);
+
+  std::size_t s = tensor_policy::stride;
 
   if(statenp == 0) {
-    for(int i = 0; i < 6; i++) {
+    for(int i = 0; i < s; i++) {
       (*stress)[i] *= ginf;
-      for(int j=0; j<6; j++) (*tm)[i][j] *= ginf;
+      for(int j=0; j<s; j++) (*tm)[i][j] *= ginf;
     }
   }
   else {
@@ -128,35 +134,37 @@ PronyViscoElastic<Material>::integrate(Tensor *_stress, Tensor *_tm, Tensor &en,
       }
     }
 
-    for(int i = 0; i < 6; i++) {
+    for(int i = 0; i < s; i++) {
       // update stress history variables
-      statenp[i]    = expTau[0]*staten[i]    + gxxTau[0]*((*stress)[i] - staten[i+18]);
-      statenp[i+6]  = expTau[1]*staten[i+6]  + gxxTau[1]*((*stress)[i] - staten[i+18]);
-      statenp[i+12] = expTau[2]*staten[i+12] + gxxTau[2]*((*stress)[i] - staten[i+18]);
-      statenp[i+18] = (*stress)[i];
+      statenp[i]    = expTau[0]*staten[i]    + gxxTau[0]*((*stress)[i] - staten[i+3*s]);
+      statenp[i+s]  = expTau[1]*staten[i+s]  + gxxTau[1]*((*stress)[i] - staten[i+3*s]);
+      statenp[i+2*s] = expTau[2]*staten[i+2*s] + gxxTau[2]*((*stress)[i] - staten[i+3*s]);
+      statenp[i+3*s] = (*stress)[i];
       // add viscoelastic contribution to stress
-      (*stress)[i] *= ginf; 
-      (*stress)[i] += statenp[i] + statenp[i+6] + statenp[i+12];
-      for(int j=0; j<6; j++) {
-        // add viscoelastic contribution to tm 
+      (*stress)[i] *= ginf;
+      (*stress)[i] += statenp[i] + statenp[i+s] + statenp[i+2*s];
+      for(int j=0; j<s; j++) {
+        // add viscoelastic contribution to tm
         (*tm)[i][j] *= ginf + gxxTau[0] + gxxTau[1] + gxxTau[2];
       }
     }
   }
 }
 
-template<typename Material>
+template<typename Material, class tensor_policy>
 void
-PronyViscoElastic<Material>::integrate(Tensor *_stress, Tensor &en, Tensor &enp,
+PronyViscoElastic<Material, tensor_policy>::integrate(Tensor *_stress, Tensor &en, Tensor &enp,
                                        double *staten, double *statenp, double temp, Tensor *cache, double dt)
 {
   // compute hyperelastic response
   Material::integrate(_stress, en, enp, staten, statenp, temp, cache, dt);
 
-  Tensor_d0s2_Ss12 *stress = static_cast<Tensor_d0s2_Ss12 *>(_stress); // second P-K stress tensor
+  typename tensor_policy::d0s2_S *stress = static_cast<typename tensor_policy::d0s2_S *>(_stress); // second P-K stress tensor
+
+  std::size_t s = tensor_policy::stride;
 
   if(statenp == 0) {
-    for(int i = 0; i < 6; i++) {
+    for(int i = 0; i < s; i++) {
       (*stress)[i] *= ginf;
     }
   }
@@ -171,7 +179,7 @@ PronyViscoElastic<Material>::integrate(Tensor *_stress, Tensor &en, Tensor &enp,
         expTau[k] = 0;
         gxxTau[k] = 0;
       }
-      else { 
+      else {
         double x;
         if((x = dt/tau[k]) <= std::numeric_limits<double>::epsilon()) {
           expTau[k] = 1;
@@ -184,22 +192,22 @@ PronyViscoElastic<Material>::integrate(Tensor *_stress, Tensor &en, Tensor &enp,
       }
     }
 
-    for(int i = 0; i < 6; i++) {
-      // update stress history variables   
-      statenp[i]    = expTau[0]*staten[i]    + gxxTau[0]*((*stress)[i] - staten[i+18]);
-      statenp[i+6]  = expTau[1]*staten[i+6]  + gxxTau[1]*((*stress)[i] - staten[i+18]);
-      statenp[i+12] = expTau[2]*staten[i+12] + gxxTau[2]*((*stress)[i] - staten[i+18]);
-      statenp[i+18] = (*stress)[i];
+    for(int i = 0; i < s; i++) {
+      // update stress history variables
+      statenp[i]    = expTau[0]*staten[i]    + gxxTau[0]*((*stress)[i] - staten[i+3*s]);
+      statenp[i+s]  = expTau[1]*staten[i+2*s]  + gxxTau[1]*((*stress)[i] - staten[i+3*s]);
+      statenp[i+2*s] = expTau[2]*staten[i+3*s] + gxxTau[2]*((*stress)[i] - staten[i+3*s]);
+      statenp[i+3*s] = (*stress)[i];
       // add viscoelastic contribution to stress
-      (*stress)[i] *= ginf; 
-      (*stress)[i] += statenp[i] + statenp[i+6] + statenp[i+12];
+      (*stress)[i] *= ginf;
+      (*stress)[i] += statenp[i] + statenp[i+s] + statenp[i+2*s];
     }
   }
 }
 
-template<typename Material>
+template<typename Material, class tensor_policy>
 void
-PronyViscoElastic<Material>::print(std::ostream &out) const
+PronyViscoElastic<Material, tensor_policy>::print(std::ostream &out) const
 {
   out << "Visco";
   Material::print(out);

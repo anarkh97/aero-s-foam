d7ece70093db5b5d666ac06df65d9b94bd273b67
Michel Lesoinne <michel.lesoinne@gmail.com>
2018-02-21 16:14:44 -0700

    Modernize getTime to use chrono.

--- Timers.d/GetTime.C
+++ Timers.d/GetTime.C
@@ -1,26 +1,11 @@
+#include <chrono>
+
 #include <sys/time.h>
 #include <Timers.d/GetTime.h>
 
-#ifdef WINDOWS
-#include <sys/timeb.h>
-#endif
-
-#ifdef WINDOWS
-double getTime()
-{
-	//return 0;
-	timeb tb;
-	ftime(&tb);
-	double r = 1000.0*tb.time + tb.millitm/1000.0;
-	return r;
-}
-
-#else
-// function to get current time
+using namespace std::chrono;
 double getTime() {
- timeval tp;
- struct timezone tz;
- gettimeofday(&tp, &tz);
- return 1000.0*tp.tv_sec + tp.tv_usec/1000.0;
+	static auto t0 = high_resolution_clock::now();
+	std::chrono::duration<double> elapsed_seconds = (high_resolution_clock::now()-t0);
+	return elapsed_seconds.count()*1000;
 }
-#endif

d65d44b24025425e3807109979078462fa81347f
Philip Avery <pavery@stanford.edu>
2018-07-12 20:07:36 +0300

    Restored a policy that was inadvertently changed during a recent merge: if the mump solver is specified and the unsymmetric keyword is specified under NONLINEAR, then the mumps unsymetric factorization method is automatically activated.

--- Solvers.d/Mumps.C
+++ Solvers.d/Mumps.C
@@ -2,6 +2,7 @@
 #include <Driver.d/Communicator.h>
 #include <Solvers.d/SolverCntl.h>
 #include <Math.d/VectorSet.h>
+#include <Utils.d/SolverInfo.h>
 #include <iostream>
 #include <map>
 
@@ -10,6 +11,7 @@ inline void Tmumps_c(DMUMPS_STRUC_C &id) { dmumps_c(&id); }
 inline void Tmumps_c(ZMUMPS_STRUC_C &id) { zmumps_c(&id); }
 #endif
 
+extern SolverInfo &solInfo;
 extern long totMemMumps;
 
 #define	USE_COMM_WORLD	-987654
@@ -39,7 +41,7 @@ GenMumpsSolver<Scalar>::GenMumpsSolver(Connectivity *nToN, EqNumberer *_dsa, Sol
 
 template<class Scalar>
 GenMumpsSolver<Scalar>::GenMumpsSolver(Connectivity *nToN, DofSetArray *_dsa, ConstrainedDSA *c_dsa, SolverCntl& _scntl, FSCommunicator *_mpicomm)
- : SparseData(_dsa,c_dsa,nToN,0,1,_scntl.unsymmetric), scntl(_scntl)
+ : SparseData(_dsa,c_dsa,nToN,0,1,(_scntl.unsymmetric||solInfo.getNLInfo().unsymmetric)), scntl(_scntl)
 {
 #ifndef USE_MUMPS
   std::cerr << " *** ERROR: Solver requires AERO-S configured with the MUMPS library. Exiting...\n";
@@ -57,7 +59,8 @@ GenMumpsSolver<Scalar>::GenMumpsSolver(Connectivity *nToN, DofSetArray *_dsa, Co
 template<class Scalar>
 GenMumpsSolver<Scalar>::GenMumpsSolver(Connectivity *nToN, DofSetArray *_dsa, ConstrainedDSA *c_dsa, int nsub,
                                        GenSubDomain<Scalar> **sd, SolverCntl& _scntl, FSCommunicator *_mpicomm)
- : SparseData(_dsa,c_dsa,nToN,0,1,_scntl.unsymmetric), MultiDomainSolver<Scalar>(numUncon, nsub, sd, _mpicomm), scntl(_scntl)
+ : SparseData(_dsa,c_dsa,nToN,0,1,(_scntl.unsymmetric||solInfo.getNLInfo().unsymmetric)),
+   MultiDomainSolver<Scalar>(numUncon, nsub, sd, _mpicomm), scntl(_scntl)
 {
 #ifndef USE_MUMPS
   std::cerr << " *** ERROR: Solver requires AERO-S configured with the MUMPS library. Exiting...\n";
@@ -79,7 +82,7 @@ GenMumpsSolver<Scalar>::init()
 #ifdef USE_MUMPS
   mumpsId.id.par = 1; // 1: working host model
   mumpsId.id.sym = scntl.pivot ? 2 : 1; // 2: general symmetric, 1: symmetric positive definite, 0: unsymmetric
-  if(scntl.unsymmetric) mumpsId.id.sym = 0;
+  if(scntl.unsymmetric || solInfo.getNLInfo().unsymmetric) mumpsId.id.sym = 0;
   mumpsCPU = true;
   host = (mpicomm) ? (mpicomm->cpuNum() == 0) : true;
 #ifdef USE_MPI
@@ -210,7 +213,7 @@ GenMumpsSolver<Scalar>::add(FullSquareMatrix &kel, int *dofs)
     if(unconstrNum[dofs[i]] == -1) continue;   // Skip constrained dofs
     for(j = 0; j < kndof; ++j) {               // Loop over columns.
       if(unconstrNum[dofs[j]] == -1) continue; // Skip constrained dofs
-      if(!scntl.unsymmetric && unconstrNum[dofs[j]] < unconstrNum[dofs[i]]) continue;
+      if(!(scntl.unsymmetric||solInfo.getNLInfo().unsymmetric) && unconstrNum[dofs[j]] < unconstrNum[dofs[i]]) continue;
       mstart = xunonz[unconstrNum[dofs[j]]];
       mstop  = xunonz[unconstrNum[dofs[j]]+1];
       for(m = mstart; m < mstop; ++m) {

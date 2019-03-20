ca01a7b2b087119d264f5e1adda764c571a8de45
Philip Avery <pavery@stanford.edu>
2019-03-20 10:35:43 -0700

    Update scotch interface to work with recent change to pointer index type in Connectivity class.

--- Utils.d/Scotch.C
+++ Utils.d/Scotch.C
@@ -13,20 +13,22 @@ Connectivity::SCOTCH_graphPart(int partnbr) const
   //cerr << "here in Connectivity::SCOTCH_graphPart, partnbr = " << partnbr << ", vertToVert = \n"; print();
 #ifdef USE_SCOTCH
   SCOTCH_Graph * grafptr = new SCOTCH_Graph;
-  int baseval = 0; // the graph base value for index arrays (typically 0 for structures
-                   // built from C and 1 for structures built from Fortran).
-  int vertnbr = size; // number of vertices
-  const int *verttab = pointer.data(); // the adjacency index array, of size (vertnbr + 1) if
-                    // the edge array is compact (that is, if vendtab equals verttab + 1 or NULL),
-                    // or of size vertnbr else.
-  int * vendtab = 0; // vendtab is the adjacency end index array, of size
-                    // vertnbr if it is disjoint from verttab.
-  int * velotab = 0; // the vertex load array, of size vertnbr if it exists.
-  int * vlbltab = 0; // the vertex label array, of size vertnbr if it exists.
-  int edgenbr = target.size(); // the number of arcs (that is, twice the number of edges).
-  const int * edgetab = target.data(); // the adjacency array, of size at least edgenbr (it can be more if the
-                    // edge array is not compact).
-  int * edlotab = 0; // the arc load array, of size edgenbr if it exists
+  SCOTCH_Num baseval = 0; // the graph base value for index arrays (typically 0 for structures
+                          // built from C and 1 for structures built from Fortran).
+  SCOTCH_Num vertnbr = size; // number of vertices
+  SCOTCH_Num * verttab = new SCOTCH_Num[vertnbr + 1]; // the adjacency index array, of size (vertnbr + 1) if
+                                                      // the edge array is compact (that is, if vendtab equals verttab + 1 or NULL),
+                                                      // or of size vertnbr else.
+  for(size_t i = 0; i < vertnbr + 1; ++i) verttab[i] = pointer[i];
+  SCOTCH_Num * vendtab = 0; // vendtab is the adjacency end index array, of size
+                            // vertnbr if it is disjoint from verttab.
+  SCOTCH_Num * velotab = 0; // the vertex load array, of size vertnbr if it exists.
+  SCOTCH_Num * vlbltab = 0; // the vertex label array, of size vertnbr if it exists.
+  SCOTCH_Num edgenbr = target.size(); // the number of arcs (that is, twice the number of edges).
+  SCOTCH_Num * edgetab = new SCOTCH_Num[edgenbr]; // the adjacency array, of size at least edgenbr (it can be more if the
+                                                  // edge array is not compact).
+  for(size_t i = 0; i < edgenbr; ++i) edgetab[i] = target[i];
+  SCOTCH_Num * edlotab = 0; // the arc load array, of size edgenbr if it exists
   int ierr;
 
   // NOTE: vendtab, velotab, vlbltab and edlotab arrays are optional, and a NULL
@@ -43,7 +45,7 @@ Connectivity::SCOTCH_graphPart(int partnbr) const
   if(ierr) cerr << "ERROR: SCOTCH_stratInit unsuccessful\n";
 
   // NOTE: if SCOTCH stratGraphBipart is not called then default strategy will be used
-  int * parttab = new int[vertnbr]; // partition data (returned)
+  SCOTCH_Num * parttab = new SCOTCH_Num[vertnbr]; // partition data (returned)
 #if defined(_OPENMP)
   #pragma omp critical
 #endif
@@ -55,6 +57,8 @@ Connectivity::SCOTCH_graphPart(int partnbr) const
   Connectivity vertToPart(vertnbr, ptr, parttab);
   Connectivity *partToVert = vertToPart.alloc_reverse();
   //cerr << "partToVert = \n"; partToVert->print();
+  delete [] verttab;
+  delete [] edgetab;
   return partToVert;
 #else
   std::cerr << "USE_SCOTCH is not defined here in Connectivity::SCOTCH_graphPart\n";
